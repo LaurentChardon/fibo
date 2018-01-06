7d486d5ebc5afe682c0894af8c7992aaa2f0680d
Laurent <laurent.chardon@gmail.com>
2018-01-06 07:19:35 -0500

    Initial commit

---
+++ second/README.md
@@ -0,0 +1,7 @@
+README.md
+ Created on: Jan 6, 2018
+     Author: laurent
+
+This is the README for the project. 
+
+I should be able to include git info in here I guess.
---
+++ second/src/main.c
@@ -0,0 +1,14 @@
+/*
+ * main.c
+ *
+ *  Created on: Jan 6, 2018
+ */
+
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	printf("Hello, world!\n");
+
+	return 0;
+}
